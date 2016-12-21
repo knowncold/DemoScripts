f23e72591122e29bc438b656a0545cf35daeda41
Knowncold <llLord43@gmail.com>
2016-12-21 23:18:54 +0800

    update

---
+++ BoringBoard/index.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: knoc
+ * Date: 16-12-21
+ * Time: 下午4:29
+ */
+
+$counter = 121;
+echo "<!DOCTYPE HTML>
+        <html>
+            <link rel=\"stylesheet\" type=\"text/css\" href=\"main.css\" />
+            <body>";
+echo "<h1>无 聊 板</h1>";
+echo "<div id='quotes'>";
+echo "<p> asd</p>";
+
+
+echo "</div>";
+
+
+
+echo "<br/><div id='counter'>"."访问量:".$counter."</div>";
+echo "<div id='foot'>Powered by <a href='http://blog.KnownCold.me'>KnownCold</a></div>";
+echo "</body></html>";
